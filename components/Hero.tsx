479251c624aace10c0042ae40ef40537eb00b3e2
Rageh97 <abdelhady412rageh@gmail.com>
2023-08-06 15:04:47 +0300

    update responsive

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,13 @@ import Image from "next/image";
 import CustomButton from "./CustomButton";
 
 const Hero = () => {
-  const handleScroll = () => {};
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
+
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -17,9 +23,10 @@ const Hero = () => {
           handleClick={handleScroll}
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           title="Explore Cars"
+        
         />
       </div>
-      <div className="hero__image__container">
+      <div className="hero__image-container">
         <div className="hero__image">
           <Image src="/hero.png" fill alt="hero" className="object-contain" />
         </div>
